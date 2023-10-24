726ea4a27be020a61111eebf7e20989cc35f6574
BadgerV <segunfaozan@gmail.com>
2023-10-24 17:42:53 +0100

    Found out the use of out keywowrk in methdod parameters

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
-﻿using OOPConcept.Menu;
+﻿using System.ComponentModel.DataAnnotations;
+using OOPConcept.Menu;
 using OOPConcept.Repositories;
 using OOPConcept.Service;
 using OOPConcept.Service.Contracts;
 
-StudentRepository studentRepository = new StudentRepository();
-IStudentService studentService = new StudentService(studentRepository);
-TeacherRepository teacherRepository = new TeacherRepository();
-ITeacherService teacherService  = new TeacherService(teacherRepository);
+// StudentRepository studentRepository = new StudentRepository();
+// IStudentService studentService = new StudentService(studentRepository);
+// TeacherRepository teacherRepository = new TeacherRepository();
+// ITeacherService teacherService  = new TeacherService(teacherRepository);
 
-Menu.MainMenu(studentService, teacherService);
+// Menu.MainMenu(studentService, teacherService);
+
+int max;
+int min;
+
+
+
+void FindMinmax(int[] numbers, out int min, out int max)
+{
+    min = numbers[0];
+    max = numbers[0];
+    foreach (int num in numbers)
+    {
+        if (num < min) min = num;
+
+        if (num > max) max = num;
+    }
+
+}
+
+
+int[] numbers = { 5, 3, 9, 2, 7 };
+
+FindMinmax(numbers, out min, out max);
+
+Console.WriteLine($"{min} {max}");

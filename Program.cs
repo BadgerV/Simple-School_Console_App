e7c96380ce7d3fa09f58f765b496b54fe6384623
BadgerV <segunfaozan@gmail.com>
2023-10-25 07:19:38 +0100

    prettier:

--- Program.cs
+++ Program.cs
@@ -11,27 +11,34 @@ using OOPConcept.Service.Contracts;
 
 // Menu.MainMenu(studentService, teacherService);
 
-int max;
-int min;
+// int max;
+// int kerrey;
 
 
 
-void FindMinmax(int[] numbers, out int min, out int max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    foreach (int num in numbers)
-    {
-        if (num < min) min = num;
+// void FindMinmax(int[] numbers, out int min, out int max)
+// {
+//     min = numbers[0];
+//     max = numbers[0];
+//     foreach (int num in numbers)
+//     {
+//         if (num < min) min = num;
 
-        if (num > max) max = num;
-    }
+//         if (num > max) max = num;
+//     }
 
+// }
+
+
+// int[] numbers = { 5, 3, 9, 2, 7 };
+
+// FindMinmax(numbers, out kerrey, out max);
+
+// Console.WriteLine($"{kerrey} {max}");
+
+
+string [] newArray = new string[4];
+
+foreach(string num in newArray) {
+    Console.WriteLine($"{num}.");
 }
-
-
-int[] numbers = { 5, 3, 9, 2, 7 };
-
-FindMinmax(numbers, out min, out max);
-
-Console.WriteLine($"{min} {max}");
